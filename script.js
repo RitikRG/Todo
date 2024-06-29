f8ce5ee26b5f64c07d5d71ac2205d0b95b3dbfcb
Ritik Gupta <Ritik@Apples-MacBook-Pro.local>
2024-06-29 12:28:56 +0530

    Added responsive css, solved the mobile deletion issue

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
+const addContainer = document.getElementById("addContainer");
 const inputFeild = document.getElementById("inputFeild");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
-let taskCount = 0;
+addContainer.addEventListener('submit', (e)=>{e.preventDefault()});
 
 const addTask=()=>{
     let task = inputFeild.value;
     if(task!=""){
-        taskCount+=1;
-        let taskId = 't'+taskCount;
-        console.log(task);
+        const markup = `
+            <div class="task" >
+                <div>
+                    <input type="radio" class='completeBtn' name="done">
+                    <p>${task}</p>
+                </div>
+                <button><img src="./assets/cross.png" alt=""></button>
+            </div>
+        `;
+        taskList.innerHTML+=markup;
         inputFeild.value='';
-
-        // Creating the specific task
-        let taskDiv = document.createElement('div');
-        taskDiv.classList.add('task');
-        taskDiv.id=taskId;
-
-        let innerDiv = document.createElement('div');
-        
-
-        let completedBtn = document.createElement('input');
-        completedBtn.type='checkbox';
-        completedBtn.id='c'+taskId;
-        innerDiv.appendChild(completedBtn);
-
-        let taskDisplay = document.createElement('p');
-        taskDisplay.innerText=task;
-        innerDiv.appendChild(taskDisplay);
-
-        taskDiv.appendChild(innerDiv);
-
-        let deleteBtn = document.createElement('button');
-        let delIcon = document.createElement('i');
-        delIcon.classList.add("fa-solid");
-        delIcon.classList.add("fa-xmark");
-        deleteBtn.appendChild(delIcon);
-
-        taskDiv.appendChild(deleteBtn);
-
-        taskList.appendChild(taskDiv);
     }
     
 }
@@ -51,7 +30,7 @@ taskList.addEventListener('click',(e)=>{
     console.log(e.target.tagName);
     if(e.target.tagName === 'INPUT'){
         e.target.parentElement.classList.toggle('completed');
-    }else if(e.target.tagName=== 'BUTTON'){
-        e.target.parentElement.remove();
+    }else if(e.target.tagName=== 'IMG'){
+        e.target.parentElement.parentElement.remove();
     }
 })
